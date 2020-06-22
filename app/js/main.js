be8362ac242f625623aad83a410a350009c65d9b
Valerii2390-dev <valezh14@yahoo.com>
2020-06-22 14:23:41 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function() {
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
 
+    $('.product__slider-inner').slick({
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true
+    });
+
+
+    var mixer = mixitup('.products__inner-box');
 });
